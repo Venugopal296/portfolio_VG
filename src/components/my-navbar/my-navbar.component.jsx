0c6998ae17d98a09e5ee8375da425f1e49cd0120
Venugopal296 <50396414+Venugopal296@users.noreply.github.com>
2020-10-11 22:32:50 +0530

    Initial Commit

---
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./my-navbar.styles.css";
+
+const MyNavbar = () => {
+  return (
+    <div>
+      <Navbar
+        fixed="top"
+        variant="dark"
+        expand="md"
+        className="animate-navbar nav-theme"
+      >
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    </div>
+  );
+};
+
+export default MyNavbar;
